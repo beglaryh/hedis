91a44d1a186b1ce84dec173e3bfee499c7631544
Hrach Beglaryan <beglaryanh@gmail.com>
2024-10-11 00:28:03 -0700

    supporting simple persistence

--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
 	"redis-clone/redis_clone"
 )
 
 func main() {
+	f, _ := os.ReadFile("splash.txt")
+	fmt.Println(string(f))
+
 	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%s", "127.0.0.1", "6379"))
 	if err != nil {
 		log.Fatal(err)
--- redis_clone/value_element.go
+++ redis_clone/value_element.go
@@ -1,5 +1,11 @@
 package redis_clone
 
+import (
+	"strings"
+
+	"github.com/beglaryh/gocommon/collection/list/linkedlist"
+)
+
 type valueElement struct {
 	v  any
 	et elementType
@@ -11,3 +17,35 @@ const (
 	ESTRING elementType = iota + 1
 	ELIST
 )
+
+func (v valueElement) GetValue() any {
+	return v.v
+}
+
+func (t elementType) IsString() bool {
+	return t == ESTRING
+}
+
+func (t elementType) IsList() bool {
+	return t == ELIST
+}
+
+func (v valueElement) String() string {
+	if v.et.IsString() {
+		return v.v.(string)
+	}
+	l := v.v.(linkedlist.LinkedList[string])
+	var sb strings.Builder
+	count := 0
+	sb.WriteString("[")
+	l.Stream().ForEach(func(s string) {
+		sb.WriteString(s)
+		if count != l.Size()-1 {
+			sb.WriteString(",")
+		}
+		count += 1
+	})
+
+	sb.WriteString("]")
+	return sb.String()
+}
